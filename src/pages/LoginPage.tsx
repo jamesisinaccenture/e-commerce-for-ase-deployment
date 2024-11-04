3aaab45e6c8ab894be7da018ea721a6d1c063ae6
Mamolo <noreen.keith.mamolo@accenture.com>
2024-11-04 13:22:36 +0800

    ECASE-67-feature: Added Form component from Shadcn. Added a sample validation. Added Email and Password Input type. Installed the Shadcn Form package.

--- src/pages/LoginPage.tsx
+++ src/pages/LoginPage.tsx
@@ -1,11 +1,54 @@
 import CustomInput from "@/components/reusable/CustomInput";
+import { Button } from "@/components/ui/button";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
+import { z } from "zod";
+import { Form, FormField, FormItem, FormMessage } from "@/components/ui/form";
+
+const formSchema = z.object({
+  email: z.string().min(2, {
+    message: "Test",
+  }),
+  password: z.string().min(6, {
+    message: "Test",
+  }),
+}); //Not the final validation for the login page
 
 const LoginPage = () => {
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      email: "",
+      password: "",
+    },
+  });
+
   return (
-    <div className="bg-gray-500 text-center p-5">
-      <CustomInput label="Email Address" />
-      <CustomInput label="Username" />
-    </div>
+    <Form {...form}>
+      <form className="rounded border">
+        <FormField
+          control={form.control}
+          name="email"
+          render={({ field }) => (
+            <FormItem>
+              <CustomInput type="email" label="Email Address" {...field} />
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+        <FormField
+          control={form.control}
+          name="password"
+          render={({ field }) => (
+            <FormItem>
+              <CustomInput type="password" label="Password" {...field} />
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+        <Button type="submit">Login</Button>
+      </form>
+    </Form>
   );
 };
 
