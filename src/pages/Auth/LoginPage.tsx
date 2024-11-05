f8e280cd5dbac8774b726ad2dc35645226898342
Mamolo <noreen.keith.mamolo@accenture.com>
2024-11-05 09:17:10 +0800

    ECASE-67-feature: Added Shadcn checkbox package. Added tailwind css and some other tags/components. Such as: Checkbox, label, p, div, and img.

--- src/pages/Auth/LoginPage.tsx
+++ src/pages/Auth/LoginPage.tsx
@@ -3,6 +3,7 @@ import { Button } from "@/components/ui/button";
 import { zodResolver } from "@hookform/resolvers/zod";
 import { useForm } from "react-hook-form";
 import { z } from "zod";
+import { Checkbox } from "@/components/ui/checkbox";
 import {
   Form,
   FormDescription,
@@ -32,40 +33,59 @@ const LoginPage = () => {
 
   return (
     <Form {...form}>
-      <form className="container p-12 mt-16">
-        <div className="mb-8">
-          <FormLabel className="text-3xl font-sans font-semibold">
+      <form className="container p-12 ml-16 flex space-x-96 flex-wrap">
+        <div className="mt-16">
+          <div className="mb-8">
+            <FormLabel className="text-3xl font-sans font-semibold">
+              Login
+            </FormLabel>
+            <FormDescription className="mt-3">
+              Login to access your account.
+            </FormDescription>
+          </div>
+          <div className="mb-3 w-80">
+            <FormField
+              control={form.control}
+              name="email"
+              render={({ field }) => (
+                <FormItem>
+                  <CustomInput type="email" label="Email" {...field} />
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+          </div>
+          <div className="mb-3 w-80">
+            <FormField
+              control={form.control}
+              name="password"
+              render={({ field }) => (
+                <FormItem>
+                  <CustomInput type="password" label="Password" {...field} />
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+          </div>
+          <div className="flex items-center space-x-24 mb-8 text-sm">
+            <div>
+              <Checkbox id="rem" />
+              <label htmlFor="rem" className="ml-1">
+                Remember me
+              </label>
+            </div>
+            <p className="text-red-500">Forgot Password</p>
+          </div>
+          <Button type="submit" className="w-80 bg-blue-600">
             Login
-          </FormLabel>
-          <FormDescription className="mt-3">
-            Login to access your account.
-          </FormDescription>
+          </Button>
+          <p className="text-sm mt-2">
+            Don't have an account? <span className="text-red-500">Signup</span>
+          </p>
         </div>
-        <div className="mb-3 w-80">
-          <FormField
-            control={form.control}
-            name="email"
-            render={({ field }) => (
-              <FormItem>
-                <CustomInput type="email" label="Email" {...field} />
-                <FormMessage />
-              </FormItem>
-            )}
-          />
+        <div className="content-center">
+          <img src="sample.jpg" alt="Sample Image." />
         </div>
-        <div>
-          <FormField
-            control={form.control}
-            name="password"
-            render={({ field }) => (
-              <FormItem>
-                <CustomInput type="password" label="Password" {...field} />
-                <FormMessage />
-              </FormItem>
-            )}
-          />
-        </div>
-        <Button type="submit">Login</Button>
       </form>
     </Form>
   );
