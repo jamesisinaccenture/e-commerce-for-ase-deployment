6654cf9dc927462635da03f633906b0a97aeaeb8
Mamolo <noreen.keith.mamolo@accenture.com>
2024-11-07 17:33:47 +0800

    ECASE-74-feature: Added breadcrumb component from Shadcn. Added styling with the use of Tailwind.

--- src/pages/Store/InformationSettingsPage.tsx
+++ src/pages/Store/InformationSettingsPage.tsx
@@ -1,47 +1,70 @@
-// import { Form } from "react-router-dom";
+import { Slash } from "lucide-react";
+
 import CustomInput from "@/components/reusable/CustomInput";
+import {
+  Breadcrumb,
+  BreadcrumbItem,
+  BreadcrumbLink,
+  BreadcrumbList,
+  BreadcrumbSeparator,
+} from "@/components/ui/breadcrumb";
 import { Button } from "@/components/ui/button";
 
-// import { FormItem, FormLabel } from "@/components/ui/form";
 const InformationSettingsPage = () => {
   return (
-    <>
-      <div className="flex flex-wrap justify-center items-center">
-        <div className="shadow-lg p-10 mt-10">
-          <div className="container">
-            <h1 className="text-blue-500">Edit Your Profile</h1>
+    <form className="flex flex-wrap justify-center items-center">
+      <div className="shadow-lg p-10 mt-8 mb-10">
+        <div className="container mb-8">
+          <Breadcrumb>
+            <BreadcrumbList>
+              <BreadcrumbItem>
+                <BreadcrumbLink>
+                  <h1 className="text-blue-500">Profile</h1>
+                </BreadcrumbLink>
+              </BreadcrumbItem>
+              <BreadcrumbSeparator>
+                <Slash />
+              </BreadcrumbSeparator>
+              <BreadcrumbItem>
+                <BreadcrumbLink>
+                  <h1 className="text-blue-500">Edit Your Profile</h1>
+                </BreadcrumbLink>
+              </BreadcrumbItem>
+            </BreadcrumbList>
+          </Breadcrumb>
+        </div>
+
+        <div className="container">
+          <div className="flex space-x-5 mb-3">
+            <CustomInput type="text" label="First Name" />
+            <CustomInput type="text" label="Last Name" />
           </div>
-          <div className="container">
-            <div className="flex space-x-5">
-              <CustomInput type="text" label="First Name" />
-              <CustomInput type="text" label="Last Name" />
-            </div>
-            <div className="flex space-x-5">
-              <CustomInput type="text" label="Username" />
-              <CustomInput type="email" label="Email" />
-            </div>
-            <div className="flex space-x-5">
-              <CustomInput type="tel" label="Contact Number" />
-              <CustomInput type="text" label="Address" />
-            </div>
+          <div className="flex space-x-5 mb-3">
+            <CustomInput type="text" label="Username" />
+            <CustomInput type="email" label="Email" />
           </div>
-          <div className="container">
-            <p>Password Changes</p>
-            <div>
-              <CustomInput type="password" label="Current Password" />
-              <CustomInput type="password" label="New Password" />
-              <CustomInput type="password" label="Confirm New Password" />
-            </div>
-          </div>
-          <div className="flex container flex-row-reverse">
-            <Button className="bg-blue-500">Save Changes</Button>
-            <Button className="bg-white text-black shadow-none hover:bg-white hover:text-blue-500">
-              Cancel
-            </Button>
+          <div className="flex space-x-5 mb-3">
+            <CustomInput type="tel" label="Contact Number" />
+            <CustomInput type="text" label="Address" />
           </div>
         </div>
+        <div className="container mt-5">
+          <p className="mb-3">Password Changes</p>
+          <div className="flex flex-col gap-2">
+            <CustomInput type="password" label="Current Password" />
+            <CustomInput type="password" label="New Password" />
+            <CustomInput type="password" label="Confirm New Password" />
+          </div>
+        </div>
+
+        <div className="flex container flex-row-reverse mt-5">
+          <Button className="bg-blue-500">Save Changes</Button>
+          <Button className="bg-white text-black shadow-none hover:bg-white hover:text-blue-500">
+            Cancel
+          </Button>
+        </div>
       </div>
-    </>
+    </form>
   );
 };
 
