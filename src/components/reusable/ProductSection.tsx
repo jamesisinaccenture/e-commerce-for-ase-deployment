8a9e557d7a3e56cea58b8e791ef241edeb8bfa51
Mamolo <noreen.keith.mamolo@accenture.com>
2024-11-11 13:40:50 +0800

    ECASE-84-fix: Change ul and li tag into div.

--- src/components/reusable/ProductSection.tsx
+++ src/components/reusable/ProductSection.tsx
@@ -4,10 +4,10 @@ import { store_products } from "./DummyProductsData";
 const ProductList = () => {
   return (
     <>
-      <ul className="flex flex-wrap justify-center gap-10 mt-10 mb-10">
+      <div className="flex flex-wrap justify-center gap-10 mt-10 mb-10">
         {store_products.map((products) => {
           return (
-            <li className="flex flex-col justify-center border-2 rounded-xl hover:border-blue-500 shadowx-xl">
+            <div className="flex flex-col justify-center border-2 rounded-xl hover:border-blue-500 shadowx-xl">
               <div className="container">
                 <div className="h-48 w-44">
                   <img
@@ -23,10 +23,10 @@ const ProductList = () => {
                   <p className="text-green-600">Save</p>
                 </div>
               </div>
-            </li>
+            </div>
           );
         })}
-      </ul>
+      </div>
     </>
   );
 };
