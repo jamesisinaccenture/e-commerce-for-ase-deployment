76a7c7c4af72ea80c012a4d3c5ca4f92efd2ac9f
Agathaquennie <agathaquenniematthew@gmail.com>
2024-11-12 16:36:35 +0800

    ECASE-87-fix: added padding to content wrapper

--- src/components/reusable/ContentWrapper.tsx
+++ src/components/reusable/ContentWrapper.tsx
@@ -3,7 +3,7 @@
 const ContentWrapper = ({ children }: { children: any }) => {
   return (
     <div className="flex justify-center">
-      <div className="max-w-[80rem] w-full">{children}</div>
+      <div className="max-w-[80rem] w-full px-10">{children}</div>
     </div>
   );
 };
