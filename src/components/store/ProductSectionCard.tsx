7ec5becb244e2daa074c42829435bf26202df672
Mamolo <noreen.keith.mamolo@accenture.com>
2024-11-12 08:49:32 +0800

    ECASE-75-fix: Fix UI responsiveness of ProductSectionCard.

--- src/components/store/ProductSectionCard.tsx
+++ src/components/store/ProductSectionCard.tsx
@@ -3,13 +3,13 @@ import productImage from "@/assets/images/product-image.jpg";
 const ProductSectionCard = (products: any) => {
   return (
     <>
-      <div className="flex flex-col justify-center border-2 rounded-xl hover:border-blue-500 shadowx-xl">
-        <div className="container">
-          <div className="h-48 w-44">
+      <div className="flex flex-col justify-center border-2 rounded-xl hover:border-blue-500 shadowx-xl ">
+        <div className="container mx-auto">
+          <div className="h-48 w-48">
             <img
               src={productImage} // src={products.products.product_img}
               alt={products.products.product_name}
-              className="rounded-t-lg"
+              className="rounded-t-lg h-fit w-fit"
             />
           </div>
           <div className="flex flex-col text-sm pl-2 pr-2 pb-2 font-semibold font-sans">
