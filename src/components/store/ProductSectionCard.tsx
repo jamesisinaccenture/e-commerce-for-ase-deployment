e14b6861c54d3abe2a10f39bbfdff4f3c22e1421
norainkei <noreen.keith.mamolo@accenture.com>
2024-11-15 15:57:54 +0800

    ECASE-99-fix: Fixed responsiveness of ProductSection and ProductSectionCard. Installed a component in Shadcn, called Aspect Ratio.

--- src/components/store/ProductSectionCard.tsx
+++ src/components/store/ProductSectionCard.tsx
@@ -1,25 +1,22 @@
 import productImage from "@/assets/images/product-image.jpg";
+import { AspectRatio } from "@/components/ui/aspect-ratio";
 
 const ProductSectionCard = (products: any) => {
   return (
     <>
-      <div className="flex flex-col justify-center border-2 rounded-xl hover:border-blue-500 shadowx-xl ">
-        <div className="container mx-auto">
-          <div className="h-48 w-48">
-            <img
-              src={productImage} // src={products.products.product_img}
-              alt={products.product_name}
-              className="rounded-t-lg h-fit w-fit"
-            />
-          </div>
-          <div className="flex flex-col text-sm pl-2 pr-2 pb-2 font-semibold font-sans">
-            <p>{products.product_name}</p>
-            <p>
-              "{products.currency} {products.price}"
-            </p>
-            <hr />
-            <p className="text-green-600">Save</p>
-          </div>
+      <div className="flex flex-col justify-between border-2 rounded-xl hover:border-blue-500 shadow-xl">
+        <AspectRatio ratio={8 / 9}>
+          <img
+            src={productImage} // src={products.products.product_img}
+            alt={products.product_name}
+            className="rounded-t-lg object-contain w-full"
+          />
+        </AspectRatio>
+        <div className="flex flex-col text-sm p-2 font-semibold w-full rounded-b-lg overflow-hidden">
+          <p>{products.product_name}</p>
+          {/* <p>{`${products.currency} ${products.price}`}</p> */}
+          <p>Helloooooooooooooooooooooooo</p>
+          <hr />
         </div>
       </div>
     </>
