7c2f4be57cd8263195a8e6239ff81b54ceb6a7cf
norainkei <noreen.keith.mamolo@accenture.com>
2024-11-18 16:19:07 +0800

    ECASE-34-feature: Added new file named DashboardInfoCard into DashboardPage. Added some components from Shadn. Added styling using TailwindCSS.

--- src/components/PieChartCard.tsx
+++ src/components/PieChartCard.tsx
@@ -1,5 +1,4 @@
 "use client";
-
 import { useMemo } from "react";
 import { TrendingUp } from "lucide-react";
 import { Label, Pie, PieChart } from "recharts";
@@ -19,39 +18,28 @@ import {
   ChartTooltipContent,
 } from "@/components/ui/chart";
 
-export const description = "Visits per browser";
+export const description = "Mobile Center Statistics Summary";
 
 const chartData = [
-  { browser: "chrome", visitors: 275, fill: "var(--color-chrome)" },
-  { browser: "safari", visitors: 200, fill: "var(--color-safari)" },
-  { browser: "firefox", visitors: 287, fill: "var(--color-firefox)" },
-  { browser: "edge", visitors: 173, fill: "var(--color-edge)" },
-  { browser: "other", visitors: 190, fill: "var(--color-other)" },
+  {
+    browser: "users",
+    visitors: 375,
+    fill: "var(--color-users)",
+  },
+  { browser: "new_users", visitors: 787, fill: "var(--color-new_users)" },
 ];
 
 const chartConfig = {
   visitors: {
     label: "Visitors",
   },
-  chrome: {
-    label: "Chrome",
+  users: {
+    label: "Current Users",
     color: "hsl(var(--chart-1))",
   },
-  safari: {
-    label: "Safari",
+  new_users: {
+    label: "New Users",
     color: "hsl(var(--chart-2))",
-  },
-  firefox: {
-    label: "Firefox",
-    color: "hsl(var(--chart-3))",
-  },
-  edge: {
-    label: "Edge",
-    color: "hsl(var(--chart-4))",
-  },
-  other: {
-    label: "Other",
-    color: "hsl(var(--chart-5))",
   },
 } satisfies ChartConfig;
 
@@ -63,7 +51,7 @@ const PieChartCard = () => {
   return (
     <Card className="flex flex-col">
       <CardHeader className="items-center pb-0">
-        <CardTitle>Visits per Browser</CardTitle>
+        <CardTitle>Mobile Center Visitors Summary</CardTitle>
         <CardDescription>January - June 2024</CardDescription>
       </CardHeader>
       <CardContent className="flex-1 pb-0">
