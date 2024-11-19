dfcff56ddc4199e60835058e38dac2fc4657bfd2
Quennie1017 <agathaquenniematthew@gmail.com>
2024-11-19 15:52:22 +0800

    ECASE-56: Added add to cart button in product section card

--- src/components/store/ProductSectionCard.tsx
+++ src/components/store/ProductSectionCard.tsx
@@ -11,7 +11,7 @@ const ProductSectionCard = ({
 IProductData) => {
   return (
     <>
-      <div className="flex flex-col justify-between border-2 rounded-xl hover:border-blue-500 shadow-xl">
+      <div className="flex flex-col justify-between border-2 rounded-xl hover:border-blue-500 shadow-xl transition-all">
         <AspectRatio ratio={8 / 9}>
           <img
             src={productImage} // src={products.products.product_img}
@@ -20,10 +20,15 @@ IProductData) => {
           />
         </AspectRatio>
         <div className="flex flex-col text-sm p-2 font-semibold w-full rounded-b-lg overflow-hidden">
-          <p>{product_name}</p>
+          <p className="text-lg font-bold mb-2">{product_name}</p>
           {/* <p>{`${products.currency} ${products.price}`}</p> */}
-          <p>More details here</p>
+          <p className="text-gray-600 mb-4">More details here</p>
           <hr />
+          <button
+            className="mt-4 py-2 px-4 bg-blue-500 text-white text-sm font-semibold rounded-lg shadow-md hover:bg-blue-600 focus:outline-none focus:ring-2 focus:ring-blue-400 focus:ring-opacity-75 transition-all"
+          >
+            Add to Cart
+          </button>
         </div>
       </div>
     </>
